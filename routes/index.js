7c7b9d8ee56e0014abf51b94026af24e4e6203b5
Camille Gabrieli <aziaziazi@gmail.com>
2020-10-06 14:36:21 +0200

    add message list and new message form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var messages = [
+  {
+    author: 'bunbun',
+    message: "I'm hungry",
+    date: new Date()
+  },{
+    author: 'Coco',
+    message: "Me too",
+    date: new Date()
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message board', messages});
+});
+
+router.post('/new', function(req, res, next) {
+  console.log(req.body)
+  messages.push({
+    author: req.body.author,
+    message: req.body.message,
+    date: new Date()
+  })
+  res.redirect('/')
 });
 
 module.exports = router;
